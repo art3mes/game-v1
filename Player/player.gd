4d8fec98551adca88e9070e7ad0615e406cbe8e8
art3mes <93867313+art3mes@users.noreply.github.com>
2024-01-16 14:49:30 +0530

    workin

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var _animated_sprite = $AnimatedSprite2D
 
-func _ready():
-	_animated_sprite.play("Run")
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
---
+++ Quit.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+
+
+func _on_pressed():
+	get_tree().quit()
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
