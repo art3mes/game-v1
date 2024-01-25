d1b178e4f5af71c86af295007dfed4b46e16c62e
art3mes <93867313+art3mes@users.noreply.github.com>
2024-01-25 23:42:38 +0530

    buggy

--- scenes/collectibles.gd
+++ scenes/collectibles.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var animation_player = $AnimationPlayer
+
 @onready var points_label = $"../../../UI/Panel/PointsLabel"
 func _ready():
 	points_label.text = "Points : " + str(GlobalScript.level_parameters.points)
@@ -7,6 +9,7 @@ func _ready():
 func _on_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		queue_free()
+		animation_player.play("poof")
 		GlobalScript.level_parameters.points = GlobalScript.level_parameters.points + 1
 		points_label.text = "Points : "+ str(GlobalScript.level_parameters.points)
 
