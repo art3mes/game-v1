2523ebec0d311f85f35b452507f6410eb6a7815a
art3mes <93867313+art3mes@users.noreply.github.com>
2024-01-25 23:02:00 +0530

    player movement updates

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
-
 const SPEED = 400.0
 const JUMP_VELOCITY = -700.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite_2d = $Sprite2D
 
+# Add a variable to store the facing direction.
+var facing_direction = 1
 
 func _physics_process(delta):
 	if (velocity.x > 1 or velocity.x < -1):
 		sprite_2d.animation = "running"
+		# Update facing direction based on movement.
+		facing_direction = sign(velocity.x)
 	else:
 		sprite_2d.animation = "default"
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -24,7 +27,6 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -32,6 +34,6 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, 30)
 
 	move_and_slide()
-	
-	var isLeft = velocity.x < 0
-	sprite_2d.flip_h = isLeft
+
+	# Update sprite flip based on facing direction.
+	sprite_2d.flip_h = facing_direction < 0
