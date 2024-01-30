2b33bfdd1d2a11f202b83f7e7fc67ce39cb6e6e4
art3mes <93867313+art3mes@users.noreply.github.com>
2024-01-31 00:10:11 +0530

    temp fix ??

--- scenes/character_select.gd
+++ scenes/character_select.gd
@@ -1,7 +1,10 @@
 extends Node
 
+func _ready():
+	print("out")
 func _on_player_1_pressed():
 	GlobalScript.level_parameters.character = 1
+	print("11")
 	get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
 
 
@@ -17,4 +20,13 @@ func _on_player_3_pressed():
 
 func _on_player_4_pressed():
 	GlobalScript.level_parameters.character = 4
-	get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
+	print("4")
+	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn") 
+
+
+func _on_texture_button_pressed():
+	print("lmao") # Replace with function body.
