b89374369009442858fc27c100abc102b9d76c44
art3mes <93867313+art3mes@users.noreply.github.com>
2024-02-01 00:05:24 +0530

    yes

--- scenes/character_select.gd
+++ scenes/character_select.gd
@@ -3,7 +3,21 @@ extends Node
 func _ready():
 	print("out")
 
-
+func _on_player_1_button_pressed():
+	GlobalScript.level_parameters.character = 1
+	print("1")
+	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
+	
+func _on_player_2_button_pressed():
+	GlobalScript.level_parameters.character = 2
+	print("2")
+	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
+	
+func _on_player_3_button_pressed():
+	GlobalScript.level_parameters.character = 3
+	print("3")
+	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
+	
 func _on_player_4_pressed():
 	GlobalScript.level_parameters.character = 4
 	print("4")
@@ -14,23 +28,11 @@ func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn") 
 
 
-func _on_texture_button_pressed():
-	print("lmao") # Replace with function body.
 
 
-func _on_player_3_button_pressed():
-	GlobalScript.level_parameters.character = 3
-	print("3")
-	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
 
 
-func _on_player_2_button_pressed():
-	GlobalScript.level_parameters.character = 2
-	print("2")
-	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
 
 
-func _on_player_1_button_pressed():
-	GlobalScript.level_parameters.character = 1
-	print("1")
-	#get_tree().change_scene_to_file("res://scenes/level"+GlobalScript.level_parameters.level +".tscn")
+
+
